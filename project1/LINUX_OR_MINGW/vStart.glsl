5bc84fcd96067278ee1462709706586ccd207b29
Callum- <c.e.schofield@hotmail.com>
2014-10-05 01:39:41 +0800

    Added a bunch of stuff, including part D

--- project1/LINUX_OR_MINGW/vStart.glsl
+++ project1/LINUX_OR_MINGW/vStart.glsl
@@ -5,51 +5,39 @@ in  vec3 vNormal;
 in  vec2 vTexCoord;
 
 out vec2 texCoord;
-out vec4 color;
-
 out vec3 fN;
 out vec3 fE;
-out vec3 fL;
+out vec3 fL1, fL2;
+out float distModifier1, distModifier2;
 
-//uniform vec3 AmbientProduct, DiffuseProduct, SpecularProduct;
 uniform mat4 ModelView;
 uniform mat4 Projection;
-uniform vec4 LightPosition;
-//uniform float Shininess;
-
+uniform vec4 LightPosition1;
+uniform vec4 LightPosition2;
 
 void main()
 {
-    
-    fN = vNormal;
-    fE = vPosition.xyz;
-    fL = LightPosition.xyz;
-
-    if( LightPosition.w != 0.0 )
-    {
-	fL = LightPosition.xyz - vPosition.xyz;
-    }
-    
-    gl_Position = Projection * ModelView * vPosition;
-    
-    
     // Transform vertex position into eye coordinates
     vec3 pos = (ModelView * vPosition).xyz;
 
-
     // The vector to the light from the vertex    
-    vec3 Lvec = LightPosition.xyz - pos;
+    vec3 Lvec1 = LightPosition1.xyz - pos;
+
+    //Vector from center to the light
+    vec3 Lvec2 = LightPosition2.xyz;
+
+    // Define direction vectors for Blinn-Phong shading calculation
+    fL1 = Lvec1;   // Direction to the light source
+    fL2 = Lvec2;
+    fE = -pos;     // Direction to the eye/camera
+
     
-    //float modifier = 5.0 / length( Lvec);
-
-    // Unit direction vectors for Blinn-Phong shading calculation
-    vec3 L = normalize( Lvec );   // Direction to the light source
-    vec3 E = normalize( -pos );   // Direction to the eye/camera
-    vec3 H = normalize( L + E );  // Halfway vector
-
     // Transform vertex normal into eye coordinates (assumes scaling is uniform across dimensions)
-    vec3 N = normalize( (ModelView*vec4(vNormal, 0.0)).xyz );
-
+    fN = (ModelView*vec4(vNormal, 0.0)).xyz;
+    
+    // Determine the distance modifier. Here use a linear drop off over the distance.
+    distModifier1 = 1.0 / length( Lvec1);
+    distModifier2 = 1.0 / length( Lvec2); // Drops off 10 times slower over distance 
 
     gl_Position = Projection * ModelView * vPosition;
     texCoord = vTexCoord;
